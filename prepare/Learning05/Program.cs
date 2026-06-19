e988f8fcad906977d25d283001a72214e01cfb3b
cody613 <hew24004@byui.edu>
2026-06-18 22:32:05 -0700

    Complete Polymorphism Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Purple", 4.5));
+        shapes.Add(new Rectangle("Orange", 7, 3.2));
+        shapes.Add(new Circle("Yellow", 2));
+
+        Console.WriteLine("Shape Areas");
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()} | Area: {shape.GetArea():F2}");
+        }
     }
 }
