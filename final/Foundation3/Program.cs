fb2b1ff9e8053e38ac1415edb291be36d1c2f92d
cody613 <hew24004@byui.edu>
2026-07-18 15:36:16 -0600

    Minor edits

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,7 +1,5 @@
 using System;
 
-// fill in information
-
 class Program
 {
     static void Main(string[] args)
@@ -10,7 +8,7 @@ class Program
         Lecture lecture = new Lecture("Become a Master of Beginner Piano Chords", "A session on basic chord structures.", "20 Jul 2026", "5:00 PM", address1, "Dr. Mark Keys", 50);
 
         Address address2 = new Address("391 Glen Creek Road", "Salem", "OR", "USA");
-        Reception reception = new Reception("Recital Meet & Greet", "Beginner arrangment showcase and mingling with performers,", "5 Aug 2026", "8:00 PM", address2, "rsvp@pianoclub.com");
+        Reception reception = new Reception("Recital Meet & Greet", "Beginner arrangement showcase and mingling with performers,", "5 Aug 2026", "8:00 PM", address2, "rsvp@pianoclub.com");
 
         Address address3 = new Address("942 Oakvale Park", "Seattle", "WA", "USA");
         OutdoorGathering outdoor = new OutdoorGathering("Summer Concert Series", "Live performance by our top students.", "26 Aug 2026", "7:00 PM", address3, "Clear skies and 75 degrees");
